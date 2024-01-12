a9ab6e340f3d3de08ff247e2cc899cbfee5672fb
DeepPatel25 <dp3676991@gmail.com>
2024-01-12 11:02:25 +0530

    Http Caching Demo Completed

---
+++ API/Part 6 - Web Development/HttpCachingAPI/HttpCachingAPI/Controllers/CLDataController.cs
@@ -0,0 +1,51 @@
+﻿using HttpCachingAPI.Filters;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HttpCachingAPI.Controllers
+{
+    /// <summary>
+    /// Controller for handling CLData-related operations
+    /// </summary>
+    public class CLDataController : ApiController
+    {
+        #region Get Endpoints
+
+        /// <summary>
+        /// Action method to get data with caching enabled
+        /// </summary>
+        /// <returns>Dictionary of States with Id</returns>
+        [AllowAnonymous]
+        [Route("GetData")]
+        [CacheFilter(TimeDuration = 100)] // Apply the custom CacheFilter attribute with a caching duration of 100 seconds
+        public async Task<IHttpActionResult> GetData()
+        {
+            // Sample data to be returned
+            Dictionary<string, string> obj = new Dictionary<string, string>
+            {
+                { "1", "Punjab" },
+                { "2", "Assam" },
+                { "3", "UP" },
+                { "4", "AP" },
+                { "5", "J&K" },
+                { "6", "Odisha" },
+                { "7", "Delhi" },
+                { "9", "Karnataka" },
+                { "10", "Bangalore" },
+                { "21", "Rajesthan" },
+                { "31", "Jharkhand" },
+                { "41", "Chennai" },
+                { "51", "Jammu" },
+                { "61", "Bhubaneshwar" },
+                { "71", "Delhi" },
+                { "19", "Karnataka" }
+            };
+
+            // Return the data as an HTTP 200 OK response
+            return Ok(obj);
+        }
+
+        #endregion
+    }
+}
---
+++ API/Part 6 - Web Development/HttpCachingAPI/HttpCachingAPI/Filters/CacheFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Web.Http.Filters;
+
+namespace HttpCachingAPI.Filters
+{
+    /// <summary>
+    /// Custom cache filter attribute derived from ActionFilterAttribute
+    /// </summary>
+    public class CacheFilter : ActionFilterAttribute
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Time duration for caching in seconds
+        /// </summary>
+        public int TimeDuration { get; set; }
+
+        #endregion
+
+        #region Override Methods
+
+        /// <summary>
+        /// Override the OnActionExecuted method to set caching headers
+        /// </summary>
+        /// <param name="actionExecutedContext"></param>
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            // Create a new CacheControlHeaderValue instance to manage caching headers
+            actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue()
+            {
+                // Set the maximum age of the cached content
+                MaxAge = TimeSpan.FromSeconds(TimeDuration),
+
+                // Indicate that the cached content must be revalidated with the server before serving
+                MustRevalidate = true,
+
+                // Specify that the cached content can be stored and used by public caches
+                Public = true
+            };
+        }
+
+        #endregion
+    }
+}

de219b0a736948bc147f5165eaa00eb9f1c189a6
DeepPatel25 <dp3676991@gmail.com>
2024-01-24 16:22:57 +0530

    Base Library Demo

---
+++ API/Advanced C# Topics/C# Advance/BaseLibraryFeatureDemo/BLFeature/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyExtension;
+using System.Collections.Generic;
+
+namespace BLFeature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 10, 45, 23, 67 };
+            
+            // Extension method of list using MyExtension Library
+            list.Print();
+        }
+    }
+}
---
+++ API/Advanced C# Topics/C# Advance/BaseLibraryFeatureDemo/MyExtension/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyExtension
+{
+    public static class Class1
+    {
+        /// <summary>
+        /// Print the list elements
+        /// </summary>
+        /// <param name="list"></param>
+        public static void Print(this List<int> list)
+        {
+            Console.WriteLine("List elements ");
+
+            foreach (int i in list) Console.Write(i + " ");
+
+            Console.WriteLine();
+        }
+    }
+}

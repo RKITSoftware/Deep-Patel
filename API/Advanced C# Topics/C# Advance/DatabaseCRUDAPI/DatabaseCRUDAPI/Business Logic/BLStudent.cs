9e7fe7202f853765a791a9cc9eeb5490f9ac7a99
DeepPatel25 <dp3676991@gmail.com>
2024-01-24 16:23:19 +0530

    Database CRUD complete

---
+++ API/Advanced C# Topics/C# Advance/DatabaseCRUDAPI/DatabaseCRUDAPI/Business Logic/BLStudent.cs
@@ -0,0 +1,134 @@
+﻿using DatabaseCRUDAPI.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+
+namespace DatabaseCRUDAPI.Business_Logic
+{
+    /// <summary>
+    /// Helper class for student controller
+    /// </summary>
+    public class BLStudent
+    {
+        /// <summary>
+        /// Inserting data into the college database 
+        /// </summary>
+        /// <param name="objStudent">Student data to insert</param>
+        /// <returns>Create response</returns>
+        public static HttpResponseMessage InsertData(STU01 objStudent)
+        {
+            MySqlConnection _connection = new MySqlConnection("Server=localhost;Port=3306;Database=college;User Id=Admin;Password=gs@123;");
+
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.Connection = _connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO STU01 (U01F02, U01F03) VALUES (@Name, @Age)";
+
+                cmd.Parameters.AddWithValue("@Name", objStudent.U01F02);
+                cmd.Parameters.AddWithValue("@Age", objStudent.U01F03);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+                _connection.Clone();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Data added successfully.")
+            };
+        }
+
+        /// <summary>
+        /// Reading the data from college database and add it to list of students.
+        /// </summary>
+        /// <returns>List of students</returns>
+        public static List<STU01> ReadData()
+        {
+            MySqlConnection _connection = new MySqlConnection("Server=localhost;Port=3306;Database=college;User Id=Admin;Password=gs@123;");
+            List<STU01> lstStudent = new List<STU01>();
+
+            using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM STU01;", _connection))
+            { 
+                _connection.Open();
+
+                using(MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        lstStudent.Add(new STU01()
+                        {
+                            U01F01 = (int)reader[0],
+                            U01F02 = (string)reader[1],
+                            U01F03 = (int)reader[2]
+                        });
+                    }
+                }
+
+                _connection.Clone();
+            }
+
+            return lstStudent;
+        }
+
+        /// <summary>
+        /// Updating a student information using student id
+        /// </summary>
+        /// <param name="objStudent">Student information</param>
+        /// <returns>Update response</returns>
+        public static HttpResponseMessage UpdateStudent(STU01 objStudent)
+        {
+            MySqlConnection _connection = new MySqlConnection("Server=localhost;Port=3306;Database=college;User Id=Admin;Password=gs@123;");
+
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.Connection = _connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "UPDATE STU01 SET U01F02=@Name, U01F03=@Age WHERE U01F01=@Id";
+
+                cmd.Parameters.AddWithValue("@Id", objStudent.U01F01);
+                cmd.Parameters.AddWithValue("@Name", objStudent.U01F02);
+                cmd.Parameters.AddWithValue("@Age", objStudent.U01F03);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+                _connection.Clone();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Data updated successfully.")
+            };
+        }
+
+        /// <summary>
+        /// Deleting a record in the college database using student id
+        /// </summary>
+        /// <param name="id">Student id</param>
+        /// <returns>Delete response</returns>
+        public static HttpResponseMessage DeleteStudent(int id)
+        {
+            MySqlConnection _connection = new MySqlConnection("Server=localhost;Port=3306;Database=college;User Id=Admin;Password=gs@123;");
+
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.Connection = _connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "DELETE FROM STU01 WHERE U01F01=@Id";
+
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+                _connection.Clone();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Data deleted successfully.")
+            };
+        }
+    }
+}
---
+++ API/Advanced C# Topics/C# Advance/DatabaseCRUDAPI/DatabaseCRUDAPI/Controllers/CLStudentController.cs
@@ -0,0 +1,61 @@
+﻿using DatabaseCRUDAPI.Business_Logic;
+using DatabaseCRUDAPI.Models;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DatabaseCRUDAPI.Controllers
+{
+    /// <summary>
+    /// Student controller for handling student api endpoints
+    /// </summary>
+    [RoutePrefix("api/CLStudent")]
+    public class CLStudentController : ApiController
+    {
+        /// <summary>
+        /// Creating a student in database 
+        /// </summary>
+        /// <param name="objStudent">Student Data</param>
+        /// <returns>Student create response</returns>
+        [HttpPost]
+        [Route("Create")]
+        public HttpResponseMessage CreateStudent(STU01 objStudent)
+        {
+            return BLStudent.InsertData(objStudent);
+        }
+
+        /// <summary>
+        /// Getting all students data from database
+        /// </summary>
+        /// <returns>List of students</returns>
+        [HttpGet]
+        [Route("GetStudnetsData")]
+        public IHttpActionResult GetStudentsData()
+        {
+            return Ok(BLStudent.ReadData());
+        }
+
+        /// <summary>
+        /// Updating student data in database
+        /// </summary>
+        /// <param name="objStudent">Student data</param>
+        /// <returns>Update response</returns>
+        [HttpPut]
+        [Route("UpdateStudent")]
+        public HttpResponseMessage UpdateData(STU01 objStudent)
+        {
+            return BLStudent.UpdateStudent(objStudent);
+        }
+
+        /// <summary>
+        /// Deleting a student using student id
+        /// </summary>
+        /// <param name="id">Student id</param>
+        /// <returns>Delete response</returns>
+        [HttpDelete]
+        [Route("DeleteStudent/{id}")]
+        public HttpResponseMessage DeleteData(int id)
+        {
+            return BLStudent.DeleteStudent(id);
+        }
+    }
+}

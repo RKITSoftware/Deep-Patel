ddbf2351c8e57b1a64f5930214a8f242266d6f76
DeepPatel25 <dp3676991@gmail.com>
2024-01-29 11:19:28 +0530

    FileInfo and DirectoryInfo Implemented

--- API/Advanced C# Topics/C# Advance/FileHandlingAPI/FileHandlingAPI/Controllers/CLStudentController.cs
+++ API/Advanced C# Topics/C# Advance/FileHandlingAPI/FileHandlingAPI/Controllers/CLStudentController.cs
@@ -85,7 +85,7 @@ namespace FileHandlingAPI.Controllers
             {
                 await Request.Content.ReadAsMultipartAsync(provider);
 
-                foreach(var file in provider.FileData)
+                foreach (var file in provider.FileData)
                 {
                     var name = file.Headers.ContentDisposition.FileName;
                     name = name.Trim('"');
@@ -98,7 +98,7 @@ namespace FileHandlingAPI.Controllers
 
                 return "File written successfully";
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return $"Error :- {ex.Message}";
             }
@@ -115,5 +115,101 @@ namespace FileHandlingAPI.Controllers
         [HttpPost]
         [Route("FillData")]
         public HttpResponseMessage FillStudentDataToList(string day, string month, string year) => BLStudent.FillData(day, month, year);
+
+        /// <summary>
+        /// GET :- api/CLStudent/GetDirectoryInfo
+        /// Getting the directory information of specified path
+        /// </summary>
+        /// <param name="path">path of directory</param>
+        /// <returns>Directory information</returns>
+        [HttpGet]
+        [Route("GetDirectoryInfo")]
+        public IHttpActionResult GetDirInfo(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            // Creating a Directory
+            // dirInfo.Create();
+
+            // Creating sub directories in Deep Patel
+            // dirInfo.CreateSubdirectory("College Work");
+            // dirInfo.CreateSubdirectory("Internship Work");
+
+            // Moving Souce directory data to Destination directory.
+            // dirInfo.MoveTo(destDirName: path2);
+
+            // Deleting Empty directory
+            // dirInfo2.Delete();
+
+            // Deleting all files in Directory
+            // dirInfo2.Delete(recursive: true);
+
+            // Get all Directories
+            DirectoryInfo[] dirs = dirInfo.GetDirectories();
+
+            foreach (var dir in dirs)
+            {
+                // Some Properties of DirectoryInfo
+
+                // Console.WriteLine(dir.FullName);
+                // Console.WriteLine(dir.LastAccessTime);
+                // Console.WriteLine(dir.CreationTime);
+                // Console.WriteLine(dir.Attributes);
+                // Console.WriteLine(dir.Parent);
+                // Console.WriteLine(dir.Root);
+                // Console.WriteLine(dir.LastWriteTime);
+
+                Console.WriteLine($"Directory {dir.Name} contains {dir.GetFiles().Length} files.");
+            }
+
+            return Ok(dirs);
+        }
+
+        /// <summary>
+        /// GET :- api/CLStudent/GetFileInfo
+        /// Performing methods and operation of FileInfo classes.
+        /// </summary>
+        /// <param name="path">path of file</param>
+        /// <returns>File information</returns>
+        [HttpGet]
+        [Route("GetFileInfo")]
+        public IHttpActionResult GetFileInfo(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+
+            using (StreamWriter writer = fileInfo.CreateText())
+            {
+                writer.WriteLine("File Created");
+            }
+
+            using(StreamWriter writer = fileInfo.AppendText())
+            {
+                writer.Write("Append text");
+            }
+
+            //using (StreamReader reader = fileInfo.OpenText())
+            //{
+            //    return Ok(reader.ReadToEnd());
+            //}
+
+            // Delete a file
+            // fileInfo.Delete();
+
+            // Copy a file
+            // fileInfo.CopyTo(path2);
+
+            // Move a file
+            // fileInfo.MoveTo(path2);
+
+            //Console.WriteLine($"File name is {fileInfo.Name}");
+            //Console.WriteLine($"File creation time is {fileInfo.CreationTime.ToLongTimeString()}");
+            //Console.WriteLine($"File last access time is {fileInfo.LastAccessTime.ToLongDateString()}");
+            //Console.WriteLine($"File length is {fileInfo.Length.ToString()} Bytes");
+            //Console.WriteLine($"File extension is {fileInfo.Extension}");
+            //Console.WriteLine($"File exists: {fileInfo.Exists}");
+            //Console.WriteLine($"File last write time is {fileInfo.LastWriteTime}");
+
+            return Ok(fileInfo);
+        }
     }
 }

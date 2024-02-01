6f9d26e47de899c88647b0212e7bc28163a68df7
DeepPatel25 <dp3676991@gmail.com>
2024-02-01 12:04:40 +0530

    Custom Jwt token done

---
+++ API/Advanced C# Topics/C# Advance/CustomJWTTokenAPI/CustomJWTTokenAPI/Controllers/CLTokenController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace CustomJWTTokenAPI.Controllers
+{
+    [RoutePrefix("api/CLToken")]
+    public class CLTokenController : ApiController
+    {
+        [HttpGet]
+        [Route("token")]
+        public IHttpActionResult GenerateToken(string username, string password)
+        {
+            return Ok(new { data = GenerateToken(username) });
+        }
+
+        [HttpGet]
+        [Route("Validate")]
+        public IHttpActionResult Validate(string token) {
+            return Ok(ValidateToken(token));
+        }
+
+        private string GenerateToken(string username)
+        {
+            string Secret = "ERMN05OPLoDvbTTa/QkqLNMI7cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
+
+            byte[] key = Convert.FromBase64String(Secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, username)
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            
+            return handler.WriteToken(token);
+        }
+
+        private static ClaimsPrincipal GetPrincipal(string token)
+        {
+            string Secret = "ERMN05OPLoDvbTTa/QkqLNMI7cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
+
+            try {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+                
+                if (jwtToken == null) return null;
+                
+                byte[] key = Convert.FromBase64String(Secret);
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+                
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+                
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static string ValidateToken(string token)
+        {
+            string username = null;
+            ClaimsPrincipal principal = GetPrincipal(token);
+            
+            if (principal == null) return null;
+            
+            ClaimsIdentity identity = null;
+            
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+            
+            Claim usernameClaim = identity.FindFirst(ClaimTypes.Name);
+            username = usernameClaim.Value;
+            
+            return username;
+        }
+    }
+}
---
+++ API/Advanced C# Topics/C# Advance/CustomJWTTokenAPI/CustomJWTTokenAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomJWTTokenAPI.WebApiApplication" Language="C#" %>

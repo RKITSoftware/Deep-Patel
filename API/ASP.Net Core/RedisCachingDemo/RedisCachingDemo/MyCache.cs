7bf1791928eb555fed8bb1f1a0a75181e4830d8f
DeepPatel25 <dp3676991@gmail.com>
2024-03-05 18:03:23 +0530

    Redis Caching Demo Using Redis cloud console

---
+++ API/ASP.Net Core/RedisCachingDemo/RedisCachingDemo/MyCache.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+namespace RedisCachingDemo
+{
+    class MyCache
+    {
+        static readonly ConnectionMultiplexer redis =
+            ConnectionMultiplexer.Connect("redis-16919.c256.us-east-1-2.ec2.cloud.redislabs.com:16919,password=44tjswnPQy2IC8UE62YUxP0ec3h3gR8B");
+
+        public static IDatabase Get()
+        {
+            return redis.GetDatabase();
+        }
+    }
+}
---
+++ API/ASP.Net Core/RedisCachingDemo/RedisCachingDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RedisCachingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                MyCache.Get().StringSet("Deep" + i, "Hello");
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(MyCache.Get().StringGet("Deep"));
+            Console.ReadLine();
+        }
+    }
+}

61f2bba573a09daf67fa2c2b44f967641f0ba77f
DeepPatel25 <dp3676991@gmail.com>
2024-03-19 15:41:30 +0530

    Extension Method and Admin Helper Methods

--- API/Advanced C# Topics/C# Advance/OnlineShoppingAPI/OnlineShoppingAPI/Business Logic/BLAdmin.cs
+++ API/Advanced C# Topics/C# Advance/OnlineShoppingAPI/OnlineShoppingAPI/Business Logic/BLAdmin.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShoppingAPI.Dto;
+using OnlineShoppingAPI.Extension;
 using OnlineShoppingAPI.Mappers;
 using OnlineShoppingAPI.Models;
 using ServiceStack.Data;
@@ -99,35 +100,21 @@ namespace OnlineShoppingAPI.Business_Logic
         /// <returns>Create response</returns>
         public HttpResponseMessage Create(DtoADM01 objAdminDto)
         {
-            try
-            {
-                // PreSave
-                ADMUSR objAdminUser = AdminMapper.ToPoco(objAdminDto);
+            // PreSave
+            ADMUSR objAdminUser = PreSaveAdmin(objAdminDto);
 
-                // Validation
-                if (!IsValidAdminUser(objAdminUser))
-                    return BLHelper.ResponseMessage(HttpStatusCode.PreconditionFailed,
-                        "Details are incorrect or false.");
+            // Validation
+            if (!IsValidAdminUser(objAdminUser))
+                return BLHelper.ResponseMessage(HttpStatusCode.PreconditionFailed,
+                    "Details are incorrect or false.");
 
-                // Save
-                using (var db = _dbFactory.OpenDbConnection())
-                {
-                    // Inserting admin details
-                    db.Insert(objAdminUser.M01);
-                    db.Insert(objAdminUser.R01);
+            // Save
+            if (SaveAdmin(objAdminUser))
+                return BLHelper.ResponseMessage(HttpStatusCode.Created,
+                    "Admin Created Successfully.");
 
-                    // Return success response
-                    return BLHelper.ResponseMessage(HttpStatusCode.Created,
-                        "Admin created successfully.");
-                }
-            }
-            catch (Exception ex)
-            {
-                // Log the exception or handle it accordingly
-                BLHelper.LogError(ex);
-                return BLHelper.ResponseMessage(HttpStatusCode.InternalServerError,
-                    "An error occurred while processing the create request.");
-            }
+            return BLHelper.ResponseMessage(HttpStatusCode.OK,
+                "An error occurred while saving the admin");
         }
 
         /// <summary>
@@ -137,50 +124,13 @@ namespace OnlineShoppingAPI.Business_Logic
         /// <returns>Delete response message</returns>
         public HttpResponseMessage Delete(int id)
         {
-            // Using a try-catch block to handle any potential exceptions
-            try
-            {
-                using (var db = _dbFactory.OpenDbConnection())
-                {
-                    // Retrieve the list of admins
-                    List<ADM01> lstAdmin = db.Select<ADM01>();
+            ADM01 objAdmin = GetAdmin(id);
 
-                    // Check if there is only one admin, in which case, deny deletion
-                    if (lstAdmin.Count == 1)
-                    {
-                        return BLHelper.ResponseMessage(HttpStatusCode.Forbidden,
-                            "Server can't fulfill the request because there is only one admin.");
-                    }
+            if (objAdmin == null)
+                return BLHelper.ResponseMessage(HttpStatusCode.NotFound,
+                            "Sorry, the requested admin could not be found.");
 
-                    // Find the admin by id
-                    ADM01 adminToDelete = lstAdmin.FirstOrDefault(a => a.M01F01 == id);
-
-                    // If the admin with the given id is not found, return Not Found status
-                    if (adminToDelete == null)
-                    {
-                        return BLHelper.ResponseMessage(HttpStatusCode.NotFound,
-                            "Admin not found.");
-                    }
-
-                    // Extracting information for USR01
-                    string username = adminToDelete.M01F03.Split('@')[0];
-
-                    // Delete admin and related USR01 record
-                    db.DeleteById<ADM01>(id);
-                    db.Delete<USR01>(u => u.R01F02 == username);
-
-                    // Return success response
-                    return BLHelper.ResponseMessage(HttpStatusCode.OK,
-                        "Admin deleted successfully.");
-                }
-            }
-            catch (Exception ex)
-            {
-                // Log the exception or handle it accordingly
-                BLHelper.LogError(ex);
-                return BLHelper.ResponseMessage(HttpStatusCode.InternalServerError,
-                    "An error occurred while processing the delete request.");
-            }
+            return DeleteAdmin(objAdmin);
         }
 
         /// <summary>
@@ -229,7 +179,7 @@ namespace OnlineShoppingAPI.Business_Logic
             catch (Exception ex)
             {
                 // Log the exception or handle it accordingly
-                BLHelper.LogError(ex);
+                ex.LogException();
                 return BLHelper.ResponseMessage(HttpStatusCode.InternalServerError,
                     "An error occurred while processing the request of changing email");
             }
@@ -252,7 +202,7 @@ namespace OnlineShoppingAPI.Business_Logic
             }
             catch (Exception ex)
             {
-                BLHelper.LogError(ex);
+                ex.LogException();
                 return -1;
             }
         }
@@ -260,6 +210,16 @@ namespace OnlineShoppingAPI.Business_Logic
         #endregion
 
         #region Private Methods
+
+        /// <summary>
+        /// Mapping Admin Dto to Poco Model
+        /// </summary>
+        /// <param name="objAdminDto">Admin Dto</param>
+        /// <returns>Admin user relational model</returns>
+        private ADMUSR PreSaveAdmin(DtoADM01 objAdminDto)
+        {
+            return AdminMapper.ToPoco(objAdminDto);
+        }
 
         /// <summary>
         /// Validates admin user relational model for further processing
@@ -277,6 +237,94 @@ namespace OnlineShoppingAPI.Business_Logic
                 return false;
 
             return true;
+        }
+
+        /// <summary>
+        /// Adding Admin details to the database using Ormlite
+        /// </summary>
+        /// <param name="objAdminUser">Admin User Realtional Model</param>
+        /// <returns>True if admin inserted into database else false.</returns>
+        private bool SaveAdmin(ADMUSR objAdminUser)
+        {
+            try
+            {
+                using (var db = _dbFactory.OpenDbConnection())
+                {
+                    // Inserting admin details
+                    db.Insert(objAdminUser.M01);
+                    db.Insert(objAdminUser.R01);
+
+                    // Return success response
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.LogException();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Getting admin details from the admin Id
+        /// </summary>
+        /// <param name="adminId">Admin Id</param>
+        /// <returns>Admin if found else null.</returns>
+        private ADM01 GetAdmin(int adminId)
+        {
+            try
+            {
+                using (var db = _dbFactory.OpenDbConnection())
+                {
+                    // Retrieve the list of admins
+                    List<ADM01> lstAdmin = db.Select<ADM01>();
+
+                    // Check if there is only one admin, in which case, deny deletion
+                    if (lstAdmin.Count == 1)
+                    {
+                        return null;
+                    }
+
+                    // Find the admin by id
+                    ADM01 objAdmin = lstAdmin.FirstOrDefault(a => a.M01F01 == adminId);
+                    return objAdmin;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.LogException();
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Delete Admin details from the database.
+        /// </summary>
+        /// <param name="objAdmin">Delete Admin details</param>
+        private HttpResponseMessage DeleteAdmin(ADM01 objAdmin)
+        {
+            try
+            {
+                using (var db = _dbFactory.OpenDbConnection())
+                {
+                    // Extracting information for USR01
+                    string username = objAdmin.M01F03.Split('@')[0];
+
+                    // Delete admin and related USR01 record
+                    db.DeleteById<ADM01>(objAdmin.M01F01);
+                    db.Delete<USR01>(u => u.R01F02 == username);
+
+                    // Return success response
+                    return BLHelper.ResponseMessage(HttpStatusCode.OK,
+                        "Admin deleted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.LogException();
+                return BLHelper.ResponseMessage(HttpStatusCode.InternalServerError,
+                    "An error occurred while processing the delete request.");
+            }
         }
 
         #endregion
---
+++ API/Advanced C# Topics/C# Advance/OnlineShoppingAPI/OnlineShoppingAPI/Extension/OnlineShoppingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShoppingAPI.Business_Logic;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace OnlineShoppingAPI.Extension
+{
+    public static class OnlineShoppingExtensions
+    {
+        public static bool IsSuccessStatusCode(this HttpResponseMessage response)
+        {
+            return response.StatusCode >= HttpStatusCode.OK &&
+                response.StatusCode < HttpStatusCode.Ambiguous;
+        }
+
+        public static void LogException(this Exception exception)
+        {
+            BLHelper.LogError(exception);
+        }
+    }
+}

9b85ae59175d4c5e83945e1dc2d5a4bf4f56dbff
DeepPatel25 <dp3676991@gmail.com>
2024-04-02 16:44:04 +0530

    ORM Lite Demo

---
+++ DotNet Core Training/Final Demo/OrmLiteDemo/OrmLiteDemo/Controllers/CLCustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrmLiteDemo.Model;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using System.Data;
+
+namespace OrmLiteDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CLCustomerController : ControllerBase
+    {
+        private readonly string _connectionString;
+        private readonly IDbConnectionFactory _dbFactory;
+
+        public CLCustomerController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Default");
+            _dbFactory = new OrmLiteConnectionFactory(_connectionString, MySqlDialect.Provider);
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            using (IDbConnection db = _dbFactory.OpenDbConnection())
+            {
+                List<Customer> customers = db.Select<Customer>();
+                return Ok(customers);
+            }
+        }
+    }
+}
---
+++ DotNet Core Training/Final Demo/OrmLiteDemo/OrmLiteDemo/Model/Customer.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.DataAnnotations;
+
+namespace OrmLiteDemo.Model
+{
+    /// <summary>
+    /// Customer table for storing customer details.
+    /// Alias contains name for table in database
+    /// </summary>
+    [Alias("Customers")]
+    public class Customer
+    {
+        /// <summary>
+        /// Customer Id
+        /// AutoIncrement attribute updates id one by one.
+        /// </summary>
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Customer first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Customer last name
+        /// </summary>
+        public string LastName { get; set; }
+    }
+}

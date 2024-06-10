37e093b8e27c34922d61f2457bf978fe2d8cdd3d
DeepPatel25 <dp3676991@gmail.com>
2024-06-10 10:33:46 +0530

    DevExtreme Testing Project

---
+++ Practice/DexExtreme/Testing/Testing/script.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    const checkbox = $("#temp").dxCheckBox({
+        accessKey: "c",
+        text: "Simple Check",
+        name: "MyCheckBox",
+        elementAttr: {
+            class: "checkMe"
+        },
+        height: "50px",
+        width: "120px",
+        enableThreeStateBehavior: false,
+        value: true,
+        focusStateEnabled: true,
+        hint: "You Need To Check This",
+        rtlEnabled: true,
+        readOnly: false,
+        visible: true
+    }).dxCheckBox("instance");
+});
